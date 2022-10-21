fa8ab47c97be2bbbd20847194bc26a480e3570d1
Sergey Moshnikov <s.moshnikov@gmail.com>
2022-10-21 06:04:24 +0000

    Everything seems to be working

--- src/components/TheCanvas.vue
+++ src/components/TheCanvas.vue
@@ -1,9 +1,26 @@
 <template>
-  <div><p>Here will be my canvas</p></div>
+  <div id="canvas"></div>
 </template>
 
 <script lang="ts">
+import p5 from 'p5'
+
 export default {
   name: 'MainCanvas',
+  mounted() {
+    const s = (p: p5) => {
+      p.setup = function () {
+        p.createCanvas(700, 410)
+      }
+
+      p.draw = function () {
+        p.background(0)
+        p.fill(255)
+        p.rect(100, 100, 50, 50)
+      }
+    }
+
+    const myp5 = new p5(s)
+  },
 }
 </script>

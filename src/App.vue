ecf39f2cc97b227ca21ef172dbc306a04579d9a2
Sergey Moshnikov <s.moshnikov@gmail.com>
2022-10-22 06:22:25 +0000

    Visualize the array of numbers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <the-canvas />
+  <the-canvas :data-points="dataPoints" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TheCanvas from './components/TheCanvas.vue'
 
+function generateDataPoints(n: number, maxValue: number) {
+  return [...new Array(n).keys()].map(() => Math.random() * maxValue)
+}
+
 export default defineComponent({
   name: 'App',
   components: {
     TheCanvas,
+  },
+  data() {
+    return { dataPoints: generateDataPoints(200, 1) }
   },
 })
 </script>
--- src/components/TheCanvas.vue
+++ src/components/TheCanvas.vue
@@ -6,23 +6,43 @@
 import p5 from 'p5'
 import { defineComponent } from 'vue'
 
-const CANVAS_SIZE = { width: 600, height: 400 }
+const CANVAS_SIZE = { width: 900, height: 400 }
+const CANVAS_PADDING = 6
+const BAR_PADDING = 2
 
-type ComponentData = {
-  canvas: p5 | undefined
+const DRAWABLE_AREA = {
+  width: CANVAS_SIZE.width - CANVAS_PADDING * 2,
+  height: CANVAS_SIZE.height - CANVAS_PADDING * 2,
 }
 
-const values = [1, 2, 3, 4, 5]
+type ComponentData = {
+  canvas?: p5
+}
 
 export default defineComponent({
   name: 'MainCanvas',
+  props: {
+    dataPoints: { type: Array<number>, required: true },
+  },
 
   data(): ComponentData {
-    return { canvas: undefined }
+    return {
+      canvas: undefined,
+    }
   },
 
   mounted() {
     this.canvas = this.createCanvas()
+  },
+
+  computed: {
+    maxValue() {
+      return Math.max(...this.dataPoints)
+    },
+
+    barWidth() {
+      return DRAWABLE_AREA.width / this.dataPoints.length - BAR_PADDING
+    },
   },
 
   methods: {
@@ -36,13 +56,38 @@ export default defineComponent({
       return new p5(s, this.$refs.canvas as HTMLElement)
     },
 
+    forEachDataPoint(callback: (index: number, value: number) => void) {
+      for (let i = 0; i < this.dataPoints.length; i++) {
+        callback(i, this.dataPoints[i])
+      }
+    },
+
     draw() {
       if (!this.canvas) return
       this.canvas.background(220)
-      this.drawBarchart()
+
+      this.drawBarchart(this.canvas)
     },
 
-    drawBarchart() {},
+    pickValueColor(canvas: p5, value: number) {
+      const hue = (255 * value) / this.maxValue
+      return canvas.color(`hsb(${hue.toFixed(0)}, 100%, 100%)`)
+    },
+
+    drawBarchart(canvas: p5) {
+      this.forEachDataPoint((i, value) => {
+        const startX = CANVAS_PADDING + i * (this.barWidth + BAR_PADDING)
+        const barHeight = (DRAWABLE_AREA.height * value) / this.maxValue
+
+        const startY = CANVAS_SIZE.height - barHeight - CANVAS_PADDING
+
+        const color = this.pickValueColor(canvas, value)
+        canvas.stroke(color)
+        canvas.fill(color)
+
+        canvas.rect(startX, startY, this.barWidth, barHeight)
+      })
+    },
   },
 })
 </script>

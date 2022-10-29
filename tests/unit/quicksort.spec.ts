09bc7be4d12e1e6b88c5b38f356e5d287b28d54a
Sergey Moshnikov <s.moshnikov@gmail.com>
2022-10-29 05:15:27 +0000

    Quicksort converted into generator

--- tests/unit/quicksort.spec.ts
+++ tests/unit/quicksort.spec.ts
@@ -1,30 +1,22 @@
 type Index = number
 
-function swap(array: unknown[], i: number, j: number) {
+function swap(array: any[], i: Index, j: Index) {
   const temp = array[i]
   array[i] = array[j]
   array[j] = temp
 }
 
-function scanFromStart<T>(data: T[], start: Index, end: Index, pivot: T) {
-  let i = start
-  while (data[i] < pivot && i < end) i++
-  return i
-}
+function* partition<T>(data: T[], start: Index, end: Index) {
+  function* compare(k: Index, l: Index) {
+    yield [k, l]
+    return data[k] < data[l]
+  }
 
-function scanFromEnd<T>(data: T[], start: number, end: number, pivot: T) {
-  let i = end
-  while (pivot < data[i] && i > start) i--
-  return i
-}
-
-function partition<T>(data: T[], start: Index, end: Index) {
-  const pivot = data[start]
   let i = start + 1
   let j = end
   while (true) {
-    i = scanFromStart(data, i, end, pivot)
-    j = scanFromEnd(data, start, j, pivot)
+    while ((yield* compare(i, start)) && i < end) i++
+    while ((yield* compare(start, j)) && j > start) j--
     if (i >= j) break
     swap(data, i, j)
   }
@@ -32,25 +24,28 @@ function partition<T>(data: T[], start: Index, end: Index) {
   return j
 }
 
-function quicksort<T>(data: T[], start: Index, end: Index) {
+function* quicksort<T>(
+  data: T[],
+  start: Index,
+  end: Index
+): Generator<number[]> {
   if (end <= start) return
-  const i = partition(data, start, end)
-  quicksort(data, start, i - 1)
-  quicksort(data, i + 1, end)
+  const i = yield* partition(data, start, end)
+  yield* quicksort(data, start, i - 1)
+  yield* quicksort(data, i + 1, end)
 }
 
 describe('Quicksort implementation', () => {
   it('does the partitioning in one go', () => {
     const data = [...'KRATELEPUIMQCXOS']
 
-    let index = partition(data, 0, data.length - 1)
+    for (let x of partition(data, 0, data.length - 1));
     expect(toString(data)).toEqual('ECAIEKLPUTMQRXOS')
-    expect(index).toEqual(5)
   })
 
   it('sorts the subranges', () => {
     const data = [...'KRATELEPUIMQCXOS']
-    quicksort(data, 0, data.length - 1)
+    for (let x of quicksort(data, 0, data.length - 1));
     expect(toString(data)).toEqual('ACEEIKLMOPQRSTUX')
   })
 })
